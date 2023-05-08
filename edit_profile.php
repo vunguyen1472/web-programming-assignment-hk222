abb29e1ba2df6688df3622ec89461bec912709d6
Tran Ngoc Minh Diep <67629587+dieptranngocminh@users.noreply.github.com>
2023-05-08 14:58:14 +0700

    update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -11,46 +11,57 @@
         <div class="row col-md-8 border rounded mx-auto mt-5 p-2 shadow-1g">
             <div class="col-md-4 text-center">
                 <img src="./images/anne-hathaway.jpg" class="img-fluid rounded" style="width: 180px; height:180px; height:180px; object-fit: cover;">
-                <div>
+                <!-- <div>
                     <div class="mb-3">
                         <label for="formFile" class="form-label">Click below to select the image</label>
                         <input class="form-control" type="file" id="formFile">
                     </div>
-                </div>
+                </div> -->
             </div>
             <div class="col-md-8">
                 <div class="h1">Edit Profile</div>
-                <table class="table">
-                    <tr><th colspan="2">User Details:</th></tr>
-                    <tr><th>Full name</th>
-                        <td>
-                            <input type="text" class="form-control" name="name" placeholder="Full name">
-                        </td>
-                    </tr>
-                    <tr><th>ID</th>
-                        <td>
-                            <input type="text" class="form-control" name="id" placeholder="ID">
-                        </td>
-                    </tr>
-                    <tr><th>Gender</th>
-                        <td>
-                            <select class="form-select form-select mb-3" aris-label=".form-select-lg example">
-                                <option selecte value="">Select Gender</option>
-                                <option value="Female">Female</option>
-                                <option value="Male">Male</option>
-                            </select>
-                        </td>
-                    </tr>
-                    <tr><th>Phone number</th>
-                        <td>
-                            <input type="text" class="form-control" name="phone" placeholder="Phone number">
-                        </td>
-                    </tr>
-                </table>
+                <form method="post">
+                    <table class="table table-striped">
+                        <tr><th colspan="2">User Details:</th></tr>
+                        <tr><th>Full name</th>
+                            <td>
+                                <input type="text" class="form-control" name="name" placeholder="Full name">
+                            </td>
+                        </tr>
+                        <tr><th>ID</th>
+                            <td>
+                                <input type="text" class="form-control" name="id" placeholder="ID">
+                            </td>
+                        </tr>
+                        <tr><th>Gender</th>
+                            <td>
+                                <select class="form-select form-select mb-3" aris-label=".form-select-lg example">
+                                    <option selecte value="">Select Gender</option>
+                                    <option value="Female">Female</option>
+                                    <option value="Male">Male</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr><th>Phone number</th>
+                            <td>
+                                <input type="text" class="form-control" name="phone" placeholder="Phone number">
+                            </td>
+                        </tr>
+                        <tr><th>Address</th>
+                            <td>
+                                <input type="text" class="form-control" name="address" placeholder="Address">
+                            </td>
+                        </tr>
+                    
+                    </table>
 
-                <div class="p-2">
-
-                </div>
+                    <div class="p-2">
+                        <button class="btn btn-primary float-end">Save</button>
+                        <a href="profile.php">
+                            <label class="btn btn-secondary">Back</button>
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
     </body>
