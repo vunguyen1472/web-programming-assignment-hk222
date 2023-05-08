3734e120820ba3ca43f245edb88ebdd203a514f6
Dang Tran Dat <77563214+trandat04102000@users.noreply.github.com>
2023-05-08 15:49:43 +0700

    Homepage changing

--- homepage.php
+++ homepage.php
@@ -13,15 +13,13 @@
 		<link rel="stylesheet" type="text/css" href="homepage/pater/pater.css" />
 		<script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
 	</head>
-	<body class="animation">
-		<main>
-			<div class="content">
-				<canvas class="scene scene--full" id="scene"></canvas>
-				<div class="content__inner">
-					<h2 class="content__title">ENTERPRISE</h2>
-				</div>
+	<body>
+		<div class="content animation" >
+			<canvas class="scene scene--full" id="scene"></canvas>
+			<div class="content__inner">
+				<h2 class="content__title">ENTERPRISE</h2>
 			</div>
-		</main>
+		</div>
 		<script src="homepage/js/main.js"></script>
 		<script src="homepage/js/three.min.js"></script>
 		<script src="homepage/js/perlin.js"></script>
@@ -29,3 +27,4 @@
 		<script src="homepage/js/animation.js"></script>
 	</body>
 </html>
+
