aeeabfaef2d042e641549859c3daec73f60fae8f
Tran Ngoc Minh Diep <67629587+dieptranngocminh@users.noreply.github.com>
2023-05-08 12:49:51 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="login.css">
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="./css/login.css">
     </head>
     <body>
         <div class="form">
