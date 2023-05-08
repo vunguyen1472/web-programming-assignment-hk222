7735e81f77e5fd6e9ef3ec27a1aa5ecdec577e65
Dang Tran Dat <77563214+trandat04102000@users.noreply.github.com>
2023-05-08 15:40:24 +0700

    Homepage adding

--- homepage.php
+++ homepage.php
@@ -1,7 +1,31 @@
-<div class="d-flex justify-content-center">
-    <!-- <img src="images\Task Management.jpg" alt="Homepage picture"> -->
-    This is homepage
-</div>
-
-
-
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="A collection of decorative animated background shapes powered by WebGL and TweenMax." />
+		<meta name="keywords" content="webgl, background, shape, web design, web development, tweenmax, gsap, animation" />
+		<meta name="author" content="Louis Hoebregts for Codrops" />
+		<link rel="shortcut icon" href="favicon.ico">
+		<link href="https://fonts.googleapis.com/css?family=Barlow:400,500,700|Poppins:600" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="homepage/css/base.css" />
+		<link rel="stylesheet" type="text/css" href="homepage/css/additional.css" />
+		<link rel="stylesheet" type="text/css" href="homepage/pater/pater.css" />
+		<script>document.documentElement.className="js";var supportsCssVars=function(){var e,t=document.createElement("style");return t.innerHTML="root: { --tmp-var: bold; }",document.head.appendChild(t),e=!!(window.CSS&&window.CSS.supports&&window.CSS.supports("font-weight","var(--tmp-var)")),t.parentNode.removeChild(t),e};supportsCssVars()||alert("Please view this demo in a modern browser that supports CSS Variables.");</script>
+	</head>
+	<body class="animation">
+		<main>
+			<div class="content">
+				<canvas class="scene scene--full" id="scene"></canvas>
+				<div class="content__inner">
+					<h2 class="content__title">ENTERPRISE</h2>
+				</div>
+			</div>
+		</main>
+		<script src="homepage/js/main.js"></script>
+		<script src="homepage/js/three.min.js"></script>
+		<script src="homepage/js/perlin.js"></script>
+		<script src="homepage/js/TweenMax.min.js"></script>
+		<script src="homepage/js/animation.js"></script>
+	</body>
+</html>
