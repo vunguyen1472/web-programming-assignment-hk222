11720e79bddd2744df6679ad80bda8e9ab96d9af
Tran Ngoc Minh Diep <67629587+dieptranngocminh@users.noreply.github.com>
2023-05-10 09:10:52 +0700

    Update login_process.php
    merge with master branch

--- login_process.php
+++ login_process.php
@@ -1,23 +1,10 @@
 <?php
     session_start();
-    
-    // Validation id format
-    $id = $_POST['id'];
-    // if (!(is_numeric($id) && intval($id) > 0)) {
-    //     $_SESSION['error'] = "Invalid id format";
-    //     header("Location: http://localhost/Assignment/homepage.php?page=login");
-    // } 
 
-    // // Validate password format
-    $password = $_POST['password'];
-    // if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/', $password)) {
-    //     $_SESSION['error'] = "Invalid pass format";
-    //     header("Location: http://localhost/index.php?page=login");
-    // }  
-
-    // Connect to database
-    // $conn = mysqli_connect('localhost', 'username', 'password', 'databasename');
     $conn = mysqli_connect('localhost', 'root', '', 'enterprise_management');
+
+    $id = $_POST['id'];
+    $password = $_POST['password'];
 
     // Get information of user from table user in database
     $sql = "SELECT * FROM user WHERE id='$id'";
@@ -26,23 +13,20 @@
 
     // Compare data (id, password)
     if ($password === $row['password']) {
-        // Validate successfully, move to home page
-        $_SESSION['login'] = "true";
-        $_SESSION['id'] = $row['id'];
-        $_SESSION['password'] = $row['password'];
+        $_SESSION['login-status'] = "Success";
+        $_SESSION['user_id'] = $row['id'];
+
         $_SESSION['name'] = $row['name'];
         $_SESSION['gender'] = $row['gender'];
         $_SESSION['phone'] = $row['phone'];
         $_SESSION['address'] = $row['address'];
-        header("Location: http://localhost/Assignment/homepage.php?page=homepage");
+        $_SESSION['password'] = $row['password'];
+
+        header("Location: ../index.php");
         exit();
     } else {
-        // Login error
-        echo "Incorrect id or password!";
-        $_SESSION['login'] = "false";
-        $_SESSION['error'] = "Incorrect id or password!";
-        header("Location: http://localhost/Assignment/homepage.php?page=login");
+        $_SESSION['login-status'] = "Incorrect Username/Password";
+        header("Location: ../index.php?page=login");
+        exit();
     }
 ?>
-
-
